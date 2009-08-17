fb9475745072541b28abeebc9237719736f86a4d
Trygve Laugstøl <trygvis@inamo.no>
2009-08-18 00:40:36 +0200

    o Adding a EMS client exec:java goal.

---
+++ incogito-ems-server/src/test/java/no/java/incogito/ems/server/EmsTestClient.java
@@ -0,0 +1,11 @@
+package no.java.incogito.ems.server;
+
+/**
+ * @author <a href="mailto:trygvis@java.no">Trygve Laugst&oslash;l</a>
+ * @version $Id$
+ */
+public class EmsTestClient {
+    public static void main(String[] args) {
+        no.java.ems.client.swing.EmsClient.main(args);
+    }
+}
