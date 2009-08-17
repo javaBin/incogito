95f765a5cc815021d3f3842d68b71162eef71507
Ole-Alexander Moy <olemoy@gmail.com>
2009-08-17 22:46:15 +0200

    Changed the height of a session in the calendar Added sitemesh exclude for */calendar url Added more styling for the calendar Removed session holder names

--- incogito-web/src/main/java/no/java/incogito/web/WebFunctions.java
+++ incogito-web/src/main/java/no/java/incogito/web/WebFunctions.java
@@ -32,23 +32,15 @@ public class WebFunctions {
 
     public static final F<CssConfiguration, F<List<Room>, List<String>>> generateCss = curry(new F2<CssConfiguration, List<Room>, List<String>>() {
         public List<String> f(CssConfiguration cssConfiguration, List<Room> roomList) {
-            List<String> rooms = roomList.zipIndex().map(P2.tuple(roomToCss.f(cssConfiguration)));
-
+          
             List<String> sessions = Functions.List_product(list("09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19"),
                     list("00", "15", "30", "45"), P.<String, String>p2()).
                     zipIndex().
                     map(hourToSessionCss.f(cssConfiguration));
 
-            Stream<String> durations = stream(15, 60).zapp(Stream.repeat(durationToCss.f(cssConfiguration)));
+            Stream<String> durations = stream(10, 60).zapp(Stream.repeat(durationToCss.f(cssConfiguration)));
 
-            return List.join(list(rooms, sessions, durations.toList()));
-        }
-    });
-
-    // div.room.r1 { left: 0; position: absolute; }
-    public static final F<CssConfiguration, F<Room, F<Integer, String>>> roomToCss = curry(new F3<CssConfiguration, Room, Integer, String>() {
-        public String f(CssConfiguration cssConfiguration, Room room, Integer integer) {
-            return ".room" + integer + " {left: " + (integer * cssConfiguration.emPerRoom) + "em; position: absolute;}";
+            return List.join(list(sessions, durations.toList()));
         }
     });
 
@@ -56,7 +48,7 @@ public class WebFunctions {
     public static final F<CssConfiguration, F<Integer, String>> durationToCss = curry(new F2<CssConfiguration, Integer, String>() {
         public String f(CssConfiguration cssConfiguration, Integer minutes) {
             // div.session.d15 { height: 1em; margin: 0; padding: 0; }
-            return ".duration" + minutes + " { height: " + cssConfiguration.getHeightInEm(minutes) + "em; margin: 0; padding: 0; }";
+            return ".duration" + minutes + " { height: 10em; margin: 0; padding: 0; }";
         }
     });
 
@@ -70,7 +62,7 @@ public class WebFunctions {
         F<P2<String, String>, String> prepend = P2.tuple(Functions.prepend);
 
         public String f(CssConfiguration cssConfiguration, P2<P2<String, String>, Integer> p) {
-            double em = cssConfiguration.sessionEmStart + (cssConfiguration.getHeightInEm(p._2() * 15));
+            double em = cssConfiguration.sessionEmStart + (cssConfiguration.getHeightInEm(p._2() * 10));
             return ".start" + prepend.f(p._1()) + " { top: " + oneDigitFormat.format(em) + "em; }";
         }
     });
